e23bbf10f40714d03b7b1aceec84431f5a2c71d4
Dok11 <vorobev1488@gmail.com>
2019-09-25 01:58:35 +0300

    CameraDeltas. New prepare dataset script
    1. Images saves in different field
       for access by reference in training script.
    2. Dataset size increased by made more wide window
       for calculate delta data between camera positions.

--- src/camera_deltas/prepare_data.py
+++ src/camera_deltas/prepare_data.py
@@ -1,142 +1,143 @@
 import json
 import os
-import re
+import random
 
 import numpy as np
 from keras_preprocessing.image import load_img, img_to_array
 
+
+RAD_TO_DEGREE = 57.2958  # 180/pi
 SIZE_X = 90
 SIZE_Y = 60
 CURRENT_DIR: str = os.getcwd()
 DATA_SLICE: int = 1
 MAX_COUNT: int = 1000000  # per slice
+NEIGHBOR_COUNT = 5
+ANGLE_LIMIT = 60 / RAD_TO_DEGREE
 
 DATASET = {
     'simple': {
         'images_dir': os.path.join(CURRENT_DIR, '..', '..', 'data', 'camera_deltas', 'simple_images', 'r'),
         'delta_path': os.path.join(CURRENT_DIR, '..', '..', 'data', 'camera_deltas', 'simple', 'data.json'),
-        'images_data': [],
-        'delta_data': [],
+        'files_data': [],
     },
     'classroom': {
         'images_dir': os.path.join(CURRENT_DIR, '..', '..', 'data', 'camera_deltas', 'classroom_images', 'r'),
         'delta_path': os.path.join(CURRENT_DIR, '..', '..', 'data', 'camera_deltas', 'classroom', 'data.json'),
-        'images_data': [],
-        'delta_data': [],
+        'files_data': [],
     },
 }
 
 DATA_SOURCES = [
-    # DATASET['simple'],
-    DATASET['classroom'],
+    DATASET['simple'],
+    # DATASET['classroom'],
 ]
 VALIDATION_PART = 0.2
+
+
+def get_image_as_np_array(path):
+    image_loaded = load_img(path,
+                            color_mode='grayscale',
+                            target_size=(SIZE_Y, SIZE_X),
+                            interpolation='bicubic')
+
+    image = img_to_array(image_loaded)
+
+    return np.array(image).astype('uint8')
 
 
 class DataCollector:
     def __init__(self, data_sources):
         self.data_sources = data_sources
+        self.images_np_arr = {}
 
+        # Load data from json file which generated from Blender file
+        self.load_json_data()
+
+        # Save all uses images as numpy array uint8
         self.set_images()
-        self.set_deltas()
+
+        # Collect dataset and save into npz file
         self.set_data()
+
+    def load_json_data(self):
+        for sources in self.data_sources:
+            with open(sources['delta_path'], 'r') as read_file:
+                sources['files_data'] = json.load(read_file)
 
     def set_images(self):
         for sources in self.data_sources:
-            file_list = os.listdir(sources['images_dir'])
-            sources['images_data'] = []
-
-            for file_name in file_list:
-                name_parse = re.search(r'scene(\d+)_(\d+)\.', file_name)
-
-                sources['images_data'].append({
-                    'scene': int(name_parse.group(1)),
-                    'frame': int(name_parse.group(2)),
-                    'path': os.path.join(sources['images_dir'], file_name),
-                })
-
-    def get_image_as_np_array(self, path):
-        image_loaded = load_img(path,
-                                color_mode='grayscale',
-                                target_size=(SIZE_Y, SIZE_X),
-                                interpolation='bicubic')
-
-        image = img_to_array(image_loaded)
-
-        return np.array(image).astype('uint8')
-
-    def set_deltas(self):
-        for sources in self.data_sources:
-            with open(sources['delta_path'], 'r') as read_file:
-                sources['delta_data'] = json.load(read_file)
-
-    def get_data_image_pairs(self, delta_item, images_data):
-        image_pairs = []
-
-        for image in images_data:
-            image_target = 0
-            image_previous = 0
-
-            if image['frame'] == delta_item['frame']:
-                image_target = image
-
-            if image_target:
-                image_previous = [x for x in images_data
-                                  if x['frame'] == (delta_item['frame'] - 1) and x['scene'] == image_target['scene']]
-
-                image_previous = image_previous[0]
-
-            if image_target and image_previous:
-                image_pairs.append((
-                    self.get_image_as_np_array(image_target['path']),
-                    self.get_image_as_np_array(image_previous['path']),
-                    image_target['path'],
-                    image_previous['path'],
-                ))
-
-        return image_pairs
+            for file_data in sources['files_data']:
+                image_path = os.path.join(sources['images_dir'], file_data['image_name'])
+                self.images_np_arr[image_path] = get_image_as_np_array(image_path)
 
     def get_data_for_source(self, source):
-        data = []  # [current, previous, result][]
+        data = []  # [source image, destination, fov, result][]
 
-        for delta_item in source['delta_data']:
-            image_pairs = self.get_data_image_pairs(delta_item, source['images_data'])
+        for file_data_index in range(len(source['files_data'])):
+            source_image = source['files_data'][file_data_index]
+            source_image_path = os.path.join(source['images_dir'], source_image['image_name'])
 
-            for image_pair in image_pairs:
-                data.append((
-                    image_pair[0],  # current
-                    image_pair[1],  # and previous frame
-                    (
-                        delta_item['loc']['x'],
-                        delta_item['loc']['y'],
-                        delta_item['loc']['z'],
-                        delta_item['loc']['xy'],
-                        delta_item['loc']['xz'],
-                        delta_item['loc']['yx'],
-                        delta_item['loc']['yz'],
-                        delta_item['loc']['zx'],
-                        delta_item['loc']['zy'],
-                        delta_item['rot_q']['w'],
-                        delta_item['rot_q']['x'],
-                        delta_item['rot_q']['y'],
-                        delta_item['rot_q']['z'],
-                        delta_item['rot_e']['x'],
-                        delta_item['rot_e']['y'],
-                        delta_item['rot_e']['z'],
-                    )
-                ))
+            files_to_data = []
+
+            # Find neighbors at left from source image in backward loop
+            for i in range(NEIGHBOR_COUNT)[::-1]:
+                destination = source['files_data'][file_data_index - i - 1]
+
+                if destination and destination['scene'] == source_image['scene']:
+                    files_to_data.append(destination)
+
+            # Find neighbors at right from source image
+            for i in range(NEIGHBOR_COUNT):
+                try:
+                    destination = source['files_data'][file_data_index + i + 1]
+
+                    if destination and destination['scene'] == source_image['scene']:
+                        files_to_data.append(destination)
+                except IndexError:
+                    print('out of range', IndexError)
+
+            # Make delta data between source image and every destination
+            for image_data in files_to_data:
+                destination_image_path = os.path.join(source['images_dir'], image_data['image_name'])
+
+                rotation_delta = (
+                    source_image['rot_q']['w'] - image_data['rot_q']['w'],
+                    source_image['rot_q']['x'] - image_data['rot_q']['x'],
+                    source_image['rot_q']['y'] - image_data['rot_q']['y'],
+                    source_image['rot_q']['z'] - image_data['rot_q']['z'],
+
+                    source_image['rot_e']['x'] - image_data['rot_e']['x'],
+                    source_image['rot_e']['y'] - image_data['rot_e']['y'],
+                    source_image['rot_e']['z'] - image_data['rot_e']['z'],
+                )
+
+                max_rotation_delta = max(rotation_delta)
+
+                # Save delta data if rotation delta not too large
+                if max_rotation_delta < ANGLE_LIMIT:
+                    data.append((
+                        source_image_path,  # source image path
+                        destination_image_path,  # and previous frame
+                        source_image['fov'],
+                        (  # full result
+                            source_image['loc']['x'] - image_data['loc']['x'],
+                            source_image['loc']['y'] - image_data['loc']['y'],
+                            source_image['loc']['z'] - image_data['loc']['z'],
+                        ) + rotation_delta,
+                    ))
 
         return data
 
     def set_data(self):
-        data_train = []  # [current, previous, result][]
-        data_valid = []  # [current, previous, result][]
+        data_train = []  # [source image, destination, fov, result][]
+        data_valid = []  # [source image, destination, fov, result][]
 
         for sources in self.data_sources:
             data = self.get_data_for_source(sources)
 
             for data_index in range(len(data)):
-                if data_index % (1 / VALIDATION_PART) == 0:
+                if random.uniform(0, 1) <= VALIDATION_PART:
                     data_valid.append(data[data_index])
                 else:
                     data_train.append(data[data_index])
@@ -149,7 +150,7 @@ class DataCollector:
             os.mkdir(train_dir)
 
         file = os.path.join(train_dir, 'data_' + str(SIZE_X) + 'x' + str(SIZE_Y) + '_' + slice_str)
-        np.savez(file, train=data_train, valid=data_valid)
+        np.savez(file, train=data_train, valid=data_valid, images=self.images_np_arr)
 
 
 if __name__ == '__main__':
