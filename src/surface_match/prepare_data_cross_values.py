dd89fc9d99a1c21ea1982072d596d25660569419
Dok11 <vorobev1488@gmail.com>
2019-12-01 02:58:50 +0300

    SurfaceMatch. Update prepare data script

--- src/surface_match/prepare_data_cross_values.py
+++ src/surface_match/prepare_data_cross_values.py
@@ -36,63 +36,101 @@ DATA_SOURCES = [
 ]
 
 
-for dataset in DATA_SOURCES:
-    filepath = os.path.join(CURRENT_DIR, '..', '..', 'data', 'surface_match', dataset['code'] + '.json')
+class DataCollector:
+    def __init__(self, data_sources):
+        for dataset in DATA_SOURCES:
+            filepath = os.path.join(CURRENT_DIR, '..', '..', 'data', 'surface_match', dataset['code'] + '.json')
 
-    # TODO: Get exist file with calculated value from json file
-    if os.path.exists(filepath):
-        with open(filepath) as json_file:
-            data = json.load(json_file)
-    else:
-        data = []
+            # Get exist file with calculated value from json file
+            data = self.get_exist_file_data(filepath)
 
-    # Collect folders with files
-    scenes = []
-    folders = os.listdir(dataset['images_dir'])
-    for folder in folders:
-        if os.path.isdir(os.path.join(dataset['images_dir'], folder)):
-            scenes.append(folder)
+            # Collect folders with files
+            scenes = self.get_scenes(dataset['images_dir'])
 
-    # Collect image files
-    image_files = []
-    for scene in scenes:
-        match_scene = re.findall(r'-(\d+)', scene)
+            # Collect image files
+            image_files = self.get_image_files(scenes, dataset['images_dir'])
 
-        images = os.listdir(os.path.join(dataset['images_dir'], scene))
+            # Calculate values for image
+            counter = 0
 
-        # TODO: remove [:5]
-        for image in images[:5]:
-            match = re.findall(r'root-(\d+)_frame-(\d+)', image)
+            for image_file in image_files:
+                exist_data_filter = lambda v: v['scene'] == image_file['scene']\
+                                              and v['root'] == image_file['root']\
+                                              and v['frame'] == image_file['frame']
+                exist_data = next(filter(exist_data_filter, data), None)
+                if exist_data:
+                    continue
 
-            if match:
-                image_files.append({
-                    'scene': int(match_scene[0]),
-                    'path': os.path.join(dataset['images_dir'], scene, image),
-                    'root': int(match[0][0]),
-                    'frame': int(match[0][1]),
+                print('Do calc for: ' + str(image_file))
+
+                img = Image.open(image_file['path']).convert('L')
+                arr = np.array(img)
+                flatten_arr = [x for sublist in arr for x in sublist]
+
+                pixels = len(flatten_arr)
+                white_sum = sum(flatten_arr)
+                value = white_sum / 255 / pixels
+
+                data.append({
+                    'scene': image_file['scene'],
+                    'root': image_file['root'],
+                    'frame': image_file['frame'],
+                    'value': round(value, 4),
                 })
 
-    # Calculate values for image
-    for image_file in image_files:
-        img = Image.open(image_file['path']).convert('L')
-        arr = np.array(img)
-        flatten_arr = [x for sublist in arr for x in sublist]
+                counter += 1
 
-        pixels = len(flatten_arr)
-        white_sum = sum(flatten_arr)
-        value = white_sum / 255 / pixels
+                if counter % 5000 == 0 and counter > 0:
+                    print('Save file partial')
+                    self.save_data(filepath, data)
 
-        data.append({
-            'scene': 1,
-            'root': image_file['root'],
-            'frame': image_file['frame'],
-            'value': value,
-        })
+            # Save data to json
+            print('Save file complete')
+            self.save_data(filepath, data)
 
-    # Save data to json
-    # TODO: change archviz to dataset name
-    # f = open(filepath, 'w', encoding='utf-8')
-    # json.dump(data, f, ensure_ascii=False, indent=2)
-    # f.close()
+    def get_exist_file_data(self, filepath):
+        if os.path.exists(filepath):
+            with open(filepath) as json_file:
+                return json.load(json_file)
 
-    print(data)
+        return []
+
+    def get_scenes(self, images_dir):
+        scenes = []
+        folders = os.listdir(images_dir)
+
+        for folder in folders:
+            if os.path.isdir(os.path.join(images_dir, folder)):
+                scenes.append(folder)
+
+        return scenes
+
+    def get_image_files(self, scenes, images_dir):
+        image_files = []
+
+        for scene in scenes:
+            match_scene = re.findall(r'-(\d+)', scene)
+
+            images = os.listdir(os.path.join(images_dir, scene))
+
+            for image in images:
+                match = re.findall(r'root-(\d+)_frame-(\d+)', image)
+
+                if match:
+                    image_files.append({
+                        'scene': int(match_scene[0]),
+                        'path': os.path.join(images_dir, scene, image),
+                        'root': int(match[0][0]),
+                        'frame': int(match[0][1]),
+                    })
+
+        return image_files
+
+    def save_data(self, filepath, data):
+        f = open(filepath, 'w', encoding='utf-8')
+        json.dump(data, f, ensure_ascii=False, indent=2)
+        f.close()
+
+
+if __name__ == '__main__':
+    DataCollector(DATA_SOURCES)
