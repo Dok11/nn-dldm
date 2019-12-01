7c4af8b0f4373539c084590aaa47df8010cb0173
Dok11 <vorobev1488@gmail.com>
2019-12-01 21:28:43 +0300

    SurfaceMatch. Update prepare data script and optimization it

--- src/surface_match/prepare_data_cross_values.py
+++ src/surface_match/prepare_data_cross_values.py
@@ -1,3 +1,4 @@
+import hashlib
 import json
 import os
 import re
@@ -11,6 +12,7 @@ from PIL import Image
 # on the second image from first and have structure like this:
 # [
 #   {
+#     'scene': 1,
 #     'root': '/path/to/image/root.png',
 #     'frame': '/path/to/image/frame.png',
 #     'value': 0
@@ -36,100 +38,142 @@ DATA_SOURCES = [
 ]
 
 
-class DataCollector:
-    def __init__(self, data_sources):
-        for dataset in DATA_SOURCES:
-            filepath = os.path.join(CURRENT_DIR, '..', '..', 'data', 'surface_match', dataset['code'] + '.json')
+def get_hash_image(image):
+    string = 's' + str(image['scene']) + 'r' + str(image['root']) + 'f' + str(image['frame'])
+    return hashlib.md5(string.encode('utf-8')).hexdigest()
 
-            # Get exist file with calculated value from json file
-            data = self.get_exist_file_data(filepath)
 
-            # Collect folders with files
-            scenes = self.get_scenes(dataset['images_dir'])
+class DatasetCollector:
+    def __init__(self, dataset):
+        self.dataset = dataset
 
-            # Collect image files
-            image_files = self.get_image_files(scenes, dataset['images_dir'])
+        # Path to json file with/for calculated data
+        self.filepath = os.path.join(CURRENT_DIR, '..', '..', 'data', 'surface_match', dataset['code'] + '.json')
 
-            # Calculate values for image
-            counter = 0
+        # Exist already calculated data
+        self.data = []
 
-            for image_file in image_files:
-                exist_data_filter = lambda v: v['scene'] == image_file['scene']\
-                                              and v['root'] == image_file['root']\
-                                              and v['frame'] == image_file['frame']
-                exist_data = next(filter(exist_data_filter, data), None)
-                if exist_data:
-                    continue
+        # Hash list of exist data
+        self.hash_list = []
 
-                print('Do calc for: ' + str(image_file))
+        # List of exist scene folders
+        self.scenes = []
 
-                img = Image.open(image_file['path']).convert('L')
-                arr = np.array(img)
-                flatten_arr = [x for sublist in arr for x in sublist]
+        # List of exist image files
+        self.image_files = []
 
-                pixels = len(flatten_arr)
-                white_sum = sum(flatten_arr)
-                value = white_sum / 255 / pixels
+        # Calculate values for image
+        self.calc_initial_values()
+        self.calc_values_for_images()
 
-                data.append({
-                    'scene': image_file['scene'],
-                    'root': image_file['root'],
-                    'frame': image_file['frame'],
-                    'value': round(value, 4),
-                })
+    def calc_initial_values(self):
+        # Get exist file with calculated value from json file
+        self.set_exist_file_data()
 
-                counter += 1
+        # Collect folders with files
+        self.set_scenes()
 
-                if counter % 5000 == 0 and counter > 0:
-                    print('Save file partial')
-                    self.save_data(filepath, data)
+        # Collect image files
+        self.set_image_files()
 
-            # Save data to json
-            print('Save file complete')
-            self.save_data(filepath, data)
+        # Define hash list for calculated images
+        self.set_hash_list()
 
-    def get_exist_file_data(self, filepath):
-        if os.path.exists(filepath):
-            with open(filepath) as json_file:
-                return json.load(json_file)
+    def calc_values_for_images(self):
+        counter = 0
 
-        return []
+        for image_file in self.image_files:
+            scene = image_file['scene']
+            root = image_file['root']
+            frame = image_file['frame']
+            path = image_file['path']
+            image_desc = 'Scene: ' + str(scene) + '. Root: ' + str(root) + '. Frame: ' + str(frame)
 
-    def get_scenes(self, images_dir):
-        scenes = []
-        folders = os.listdir(images_dir)
+            if self.find_image_in_data(image_file):
+                print('SKIP CALC FOR: ' + image_desc)
+                continue
+
+            print('DO CALC FOR: ' + image_desc)
+
+            img = Image.open(path).convert('L')
+            arr = np.array(img)
+            flatten_arr = [x for sublist in arr for x in sublist]
+
+            pixels = len(flatten_arr)
+            white_sum = sum(flatten_arr)
+            value = white_sum / 255 / pixels
+
+            self.data.append({
+                'scene': scene,
+                'root': root,
+                'frame': frame,
+                'value': round(value, 4),
+            })
+
+            counter += 1
+
+            if counter % 5000 == 0 and counter > 0:
+                print('Save file partial')
+                self.save_data()
+
+        # Save data to json
+        print('Save file complete')
+        self.save_data()
+
+    def set_exist_file_data(self):
+        if os.path.exists(self.filepath):
+            with open(self.filepath) as json_file:
+                self.data = json.load(json_file)
+
+    def set_scenes(self):
+        self.scenes = []
+        folders = os.listdir(self.dataset['images_dir'])
 
         for folder in folders:
-            if os.path.isdir(os.path.join(images_dir, folder)):
-                scenes.append(folder)
+            if os.path.isdir(os.path.join(self.dataset['images_dir'], folder)):
+                self.scenes.append(folder)
 
-        return scenes
+    def set_image_files(self):
+        self.image_files = []
 
-    def get_image_files(self, scenes, images_dir):
-        image_files = []
-
-        for scene in scenes:
+        for scene in self.scenes:
             match_scene = re.findall(r'-(\d+)', scene)
 
-            images = os.listdir(os.path.join(images_dir, scene))
+            images = os.listdir(os.path.join(self.dataset['images_dir'], scene))
 
             for image in images:
                 match = re.findall(r'root-(\d+)_frame-(\d+)', image)
 
                 if match:
-                    image_files.append({
+                    self.image_files.append({
                         'scene': int(match_scene[0]),
-                        'path': os.path.join(images_dir, scene, image),
+                        'path': os.path.join(self.dataset['images_dir'], scene, image),
                         'root': int(match[0][0]),
                         'frame': int(match[0][1]),
                     })
 
-        return image_files
+    def set_hash_list(self):
+        self.hash_list = []
 
-    def save_data(self, filepath, data):
-        f = open(filepath, 'w', encoding='utf-8')
-        json.dump(data, f, ensure_ascii=False, indent=2)
+        for image in self.data:
+            self.hash_list.append(get_hash_image(image))
+
+    def find_image_in_data(self, image):
+        if get_hash_image(image) in self.hash_list:
+            return True
+
+        return False
+
+    def save_data(self):
+        f = open(self.filepath, 'w', encoding='utf-8')
+        json.dump(self.data, f, ensure_ascii=False, indent=2)
         f.close()
+
+
+class DataCollector:
+    def __init__(self, data_sources):
+        for dataset in data_sources:
+            DatasetCollector(dataset)
 
 
 if __name__ == '__main__':
