a11b0b74f72ad84a182377a60ff69535fcc581c0
Dok11 <vorobev1488@gmail.com>
2019-12-18 01:57:37 +0300

    SurfaceMatch. Create file with config params

---
+++ src/surface_match/config.py
@@ -0,0 +1,8 @@
+FILE_NAME_TRAIN = 'experiment_train'
+FILE_NAME_VALID = 'experiment_valid'
+
+SIZE_X = 64  # 224 is native value
+SIZE_Y = 64  # 224 is native value
+
+IMG_SHAPE = (SIZE_Y, SIZE_X, 3)
+GROUP_COUNT = 10  # Source from src/surface_match/prepare_data.py
