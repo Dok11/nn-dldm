db88f7eb25c98b440c480f2185bf24f6290204b7
Oleg Postoev <dok@artlebedev.ru>
2019-12-24 13:43:35 +0300

    SurfaceMatch. Use GROUP_COUNT from global config.py

--- src/surface_match/create_exp_dataset.py
+++ src/surface_match/create_exp_dataset.py
@@ -2,7 +2,7 @@ import os
 
 import numpy as np
 
-from surface_match.config import FILE_NAME_VALID, FILE_NAME_TRAIN, SIZE_X, SIZE_Y
+from surface_match.config import FILE_NAME_VALID, FILE_NAME_TRAIN, SIZE_X, SIZE_Y, GROUP_COUNT
 from surface_match.dataset import get_batch, get_dataset
 
 
@@ -16,10 +16,9 @@ def save(file_name: str, x1: list, x2: list, y: list):
 (train, valid, images) = get_dataset(SIZE_X, SIZE_Y)
 
 batch_size = 30
-group_count = 10
 
-(images_1, images_2, results) = get_batch(train, images, batch_size, group_count)
+(images_1, images_2, results) = get_batch(train, images, batch_size, GROUP_COUNT)
 save(FILE_NAME_TRAIN, images_1, images_2, results)
 
-(images_1, images_2, results) = get_batch(valid, images, batch_size, group_count)
+(images_1, images_2, results) = get_batch(valid, images, batch_size, GROUP_COUNT)
 save(FILE_NAME_VALID, images_1, images_2, results)
