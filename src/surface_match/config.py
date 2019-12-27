94391eceabb2e5cb7e9959887d0feb7a20922a24
Oleg Postoev <dok@artlebedev.ru>
2019-12-27 23:42:02 +0300

    SurfaceMatch. Make external file with model

--- src/surface_match/config.py
+++ src/surface_match/config.py
@@ -1,3 +1,6 @@
+import os
+
+
 FILE_NAME_TRAIN = 'experiment_train'
 FILE_NAME_VALID = 'experiment_valid'
 
@@ -7,3 +10,7 @@ CHANNELS = 1
 
 IMG_SHAPE = (SIZE_Y, SIZE_X, CHANNELS)
 GROUP_COUNT = 10
+
+CURRENT_DIR: str = os.getcwd()
+SAVED_MODEL: str = os.path.join(CURRENT_DIR, '..', '..', 'models', 'surface_match', 'model.h5')
+SAVED_MODEL_W: str = os.path.join(CURRENT_DIR, '..', '..', 'models', 'surface_match', 'model_w.h5')
