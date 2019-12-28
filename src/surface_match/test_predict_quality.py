b1d503e82dca16db3533445bf6f8fb41a66737d0
Oleg Postoev <dok@artlebedev.ru>
2019-12-28 12:33:35 +0300

    SurfaceMatch. Script for detect extra hard examples in dataset

--- src/surface_match/test_predict_quality.py
+++ src/surface_match/test_predict_quality.py
@@ -1,3 +1,4 @@
+import hashlib
 import json
 import os
 
@@ -10,14 +11,16 @@ from surface_match.model import get_model
 
 (train, valid, images) = get_dataset(SIZE_X, SIZE_Y)
 
-train_batch_size = 30
+train_batch_size = 1000
+train_examples = 1536000
 
 model: Model = get_model()
 model.load_weights(SAVED_MODEL_W)
 model.summary()
+save_images = False
 
 hard_indexes = []
-test_batches = int(3 * 1000 * 1000 / train_batch_size)
+test_batches = int(10 * train_examples / train_batch_size)
 
 for batch in range(test_batches):
     (t_images_1, t_images_2, t_results, indexes) = get_batch(train, images, train_batch_size, GROUP_COUNT)
@@ -28,23 +31,42 @@ for batch in range(test_batches):
         predicted_result = result[index][0]
         delta = abs(predicted_result - real_result)
 
-        if delta > 0.4:
-            r_val = 'r%.2f' % real_result
-            p_val = 'p%.2f' % predicted_result
-
-            root_name = str(batch) + '-' + str(index) + '-root_' + p_val + 'vs' + r_val + '.jpg'
-            target_name = str(batch) + '-' + str(index) + '-target_' + p_val + 'vs' + r_val + '.jpg'
-
-            # save_img('bad_predictions/' + root_name, t_images_1[index])
-            # save_img('bad_predictions/' + target_name, t_images_2[index])
-
-            print('Save b:' + str(batch) + ' i:' + str(index))
+        if delta > 0.33:
+            print('Save b:' + str(batch) + '/' + str(test_batches) + ' i:' + str(index) + '/' + str(train_batch_size))
             hard_indexes.append([
                 int(indexes[index][0]),
                 int(indexes[index][1]),
                 delta
             ])
 
+            if save_images:
+                r_val = 'r%.2f' % real_result
+                p_val = 'p%.2f' % predicted_result
+
+                root_name = str(batch) + '-' + str(index) + '-root_' + p_val + 'vs' + r_val + '.jpg'
+                target_name = str(batch) + '-' + str(index) + '-target_' + p_val + 'vs' + r_val + '.jpg'
+
+                save_img('bad_predictions/' + root_name, t_images_1[index])
+                save_img('bad_predictions/' + target_name, t_images_2[index])
+
+
+print('Start save unique values')
+
+examples_hash_list = []
+unique_hard_indexes = []
+
+for i in range(len(hard_indexes)):
+    hard_example_group = hard_indexes[i][0]
+    hard_example_index = hard_indexes[i][1]
+
+    key = str(hard_example_group) + ':' + str(hard_example_index)
+    hash_key = hashlib.md5(key.encode('utf-8')).hexdigest()
+
+    if not (hash_key in examples_hash_list):
+        unique_hard_indexes.append(hard_indexes[i])
+        examples_hash_list.append(hash_key)
+
+
 f = open(os.path.join(CURRENT_DIR, 'hard_indexes.json'), 'w', encoding='utf-8')
-json.dump(hard_indexes, f, ensure_ascii=False, indent=2)
+json.dump(unique_hard_indexes, f, ensure_ascii=False, indent=2)
 f.close()
