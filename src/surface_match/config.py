a62385308f1a9a18e8509e32593a65a1a4a62b44
Oleg Postoev <dok@artlebedev.ru>
2020-01-07 12:19:29 +0300

    SurfaceMatch. Change input to (224, 224, 3)

--- src/surface_match/config.py
+++ src/surface_match/config.py
@@ -4,9 +4,9 @@ import os
 FILE_NAME_TRAIN = 'experiment_train'
 FILE_NAME_VALID = 'experiment_valid'
 
-SIZE_X = 64  # 224 is native value
-SIZE_Y = 64  # 224 is native value
-CHANNELS = 1
+SIZE_X = 224  # 224 is native value
+SIZE_Y = 224  # 224 is native value
+CHANNELS = 3
 
 IMG_SHAPE = (SIZE_Y, SIZE_X, CHANNELS)
 GROUP_COUNT = 10
