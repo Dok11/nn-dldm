dfb5af4ecda2feb8cc2abbcdfb91c9948fc18e83
Dok11 <vorobev1488@gmail.com>
2020-02-16 18:19:32 +0300

    SurfaceMatch. Update test_get_batch.py

--- src/surface_match/test_get_batch.py
+++ src/surface_match/test_get_batch.py
@@ -1,17 +1,16 @@
 import time
+import sys
+
+sys.path.append('D:/Projects/DLDM/src')
 
 from surface_match.dataset import BatchGenerator
 
 batch_generator = BatchGenerator()
 batch_generator.load_dataset()
-batch_generator.init_weights()
-batch_generator.load_example_weights()
-batch_generator.init_weight_normalize()
 
-millis = int(round(time.time() * 1000))
 # python -m cProfile -s time surface_match/test_get_batch.py
-for batch in range(100):
-    x = batch_generator.get_batch_train()
-
-end_millis = int(round(time.time() * 1000))
-print((end_millis - millis) / 1000)
+for batch in range(10):
+    millis = int(round(time.time() * 1000))
+    batch_generator.get_batch_train()
+    end_millis = int(round(time.time() * 1000))
+    print((end_millis - millis) / 1000)
