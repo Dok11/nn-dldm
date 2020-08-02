fbdd06bd4cdf901b951fc5f2b0a3731f663f5e74
Dok11 <vorobev1488@gmail.com>
2020-08-02 14:43:10 +0300

    SurfaceMatch. prepare_data.py for PyTorch

--- src/surface_match/prepare_data.py
+++ src/surface_match/prepare_data.py
@@ -3,11 +3,10 @@ import os
 import re
 
 import numpy as np
-from keras_preprocessing.image import load_img, img_to_array
-from tensorflow.python.keras.preprocessing.image import save_img
-from tensorflow.python.keras.utils import Progbar
+import pkbar
+from skimage import io, img_as_ubyte
 
-from surface_match.config import GROUP_COUNT, SIZE_Y, SIZE_X, CHANNELS
+from surface_match.config import GROUP_COUNT, SIZE_Y, SIZE_X
 
 # ============================================================================
 # --- GLOBAL PARAMS ----------------------------------------------------------
@@ -46,23 +45,11 @@ DATA_SOURCES = [
     DATASET['classroom'],
     DATASET['simple'],
 ]
-VALIDATION_PART = 0.2
 
 
 def get_image_as_np_array(path):
-    if CHANNELS == 3:
-        color_mode = 'rgb'
-    else:
-        color_mode = 'grayscale'
-
-    img_loaded = load_img(path,
-                          color_mode=color_mode,
-                          target_size=(SIZE_Y, SIZE_X),
-                          interpolation='bicubic')
-
-    img = img_to_array(img_loaded)
-
-    return np.array(img).astype('uint8')
+    image = io.imread(path)
+    return image
 
 
 class DataCollector:
@@ -88,19 +75,19 @@ class DataCollector:
         self.train_dir = os.path.join(CURRENT_DIR, '..', '..', 'train-data', 'surface_match')
 
         # ACTIONS:
-        print('Load data from json file which generated from Blender file')
+        print('\nLoad data from json file which generated from Blender file')
         self.load_json_data()
 
-        print('Save all uses images as numpy array uint8')
+        print('\nSave all uses images as numpy array uint8')
         self.set_images()
 
-        print('Set examples for dataset file')
+        print('\nSet examples for dataset file')
         self.set_examples()
 
-        print('Divide examples by distribution int the 10 groups')
+        print('\nDivide examples by distribution int the 10 groups')
         self.set_grouped_examples()
 
-        print('Collect dataset and save into npz file')
+        print('\nCollect dataset and save into npz file')
         self.save_data()
 
         print('Start saving images one by one')
@@ -140,9 +127,8 @@ class DataCollector:
         self.examples = []
 
         for source in self.data_sources:
-            print('Start set_examples from ' + source['code'])
-
-            progress_bar = Progbar(len(source['surface_match_data']))
+            counter = 0
+            pbar = pkbar.Pbar(name='\nStart set_examples from ' + source['code'], target=len(source['surface_match_data']))
 
             for item in source['surface_match_data']:
                 root_frame_num = str(item[1]).zfill(4)
@@ -154,53 +140,50 @@ class DataCollector:
                 image_target_index = self.images_dict_flip[image_target_path]
 
                 self.examples.append((image_root_index, image_target_index, item[3]))
-                progress_bar.add(1)
+                pbar.update(counter)
+                counter += 1
 
     def set_grouped_examples(self):
         self.grouped_examples = [[] for i in range(GROUP_COUNT)]
 
-        progress_bar = Progbar(len(self.examples))
+        counter = 0
+        pbar = pkbar.Pbar(name='\nset_grouped_examples', target=len(self.examples))
 
         for example in self.examples:
+            counter += 1
             example_value = example[2]
             group = round(example_value * (GROUP_COUNT - 1))
             self.grouped_examples[group].append(example)
-            progress_bar.add(1)
+            pbar.update(counter)
 
     def save_data(self):
         data_train = [[] for i in range(GROUP_COUNT)]
-        data_valid = [[] for i in range(GROUP_COUNT)]
 
         for group_index in range(len(self.grouped_examples)):
             group = self.grouped_examples[group_index]
 
             for example_index in range(len(group)):
                 example = group[example_index]
-
-                if example_index % (1 / VALIDATION_PART):
-                    data_train[group_index].append(example)
-                else:
-                    data_valid[group_index].append(example)
+                data_train[group_index].append(example)
 
         if not (os.path.isdir(self.train_dir)):
             os.mkdir(self.train_dir)
 
         file = os.path.join(self.train_dir, 'data_' + str(SIZE_X) + 'x' + str(SIZE_Y))
         np.savez_compressed(file,
-                            train=data_train,
-                            valid=data_valid,
+                            data=data_train,
                             images=self.images_np_arr)
 
     def save_img(self):
-        progress_bar = Progbar(len(self.images_np_arr))
+        pbar = pkbar.Pbar(name='\nsave_img', target=len(self.images_np_arr))
 
         for i in range(len(self.images_np_arr)):
             image = self.images_np_arr[i].astype('float32') / 255.
             file = os.path.join(self.train_dir, 'images', str(i))
-            save_img(file + '.png', image)
+            io.imsave(file + '.png', img_as_ubyte(image))
 
             if i % 100 == 0:
-                progress_bar.add(100)
+                pbar.update(i)
 
 
 if __name__ == '__main__':
